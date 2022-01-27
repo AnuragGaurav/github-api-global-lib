a3ffc5bfffe002d0054b6e5e487313c735588d7c
Anurag_1704 <54981117+AnuragGaurav@users.noreply.github.com>
2022-01-27 19:08:38 +0530

    Update lifecycle.groovy

--- vars/lifecycle.groovy
+++ vars/lifecycle.groovy
@@ -1,13 +1,43 @@
-	def clean(){
-  sh 'mvn clean'
+def compile(){
+
+echo "We are going to compile the pacakge"
+sh 'mvn compile'
 }
 
-def compile(){
-  sh 'mvn compile'
+def testing(){
+echo "We are going to test the package"
+
+// below command will check code stability.
+echo "Checking code stability"
+sh 'mvn pmd:pmd'
+
+// below command will check code coverage.
+echo "Checking code coverage"
+sh 'mvn cobertura:cobertura'
+
+// below command will check quality
+echo "Checking code quality"
+sh 'mvn checkstyle:checkstyle'
+
 }
-def stabiltity(){
-  sh 'mvn pmd:pmd'
+
+def publishReports(){
+
+recordIssues(tools: [checkStyle()])
+publishCoverage adapters: [coberturaAdapter('')], sourceFileResolver: sourceFiles('NEVER_STORE')
+recordIssues(tools: [pmdParser()])
 }
-def quality(){
-  sh 'mvn checkstyle:checkstyle'
+
+
+def cleanup(){
+
+cleanWs()
+
+}
+
+
+def cloneGit(){
+
+git branch: 'main', url: 'https://github.com/AnuragGaurav/github-api-global-lib.git'
+
 }
